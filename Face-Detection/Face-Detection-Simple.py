24f80d679015328302917ce79ece30e7181fb4a4
Alperen Aga <alperencode@gmail.com>
2023-01-17 01:36:25 +0300

    Update Face-Detection/Face-Detection-Simple

--- Face-Detection/Face-Detection-Simple.py
+++ Face-Detection/Face-Detection-Simple.py
@@ -1,32 +1,29 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
 # Using haarcascade and Starting Capture
-face_cascade = cv.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
-cap = cv.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
 
-while True:
+while cv2.waitKey(1) == -1:
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    _ , frame = cap.read()
     
     # Turning capture into gray pixels
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     # Using Face Detection Cascade
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.75, minNeighbors=5)
     
     for (x,y,w,h) in faces:
         
         # Numbers of x_start - y_start - x_end - y_end
-        print(x, y, w, h)
+        print("Face found: ",x, y, w, h)
         
         # The detected zones (Gray and colored)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
 
-        # We are saving last image photo
-        cv.imwrite("my_image.png", roi_color)
-        
         # Rectange BGR, thickness, width and height
         color = (0, 0, 255)
         border = 2
@@ -34,13 +31,12 @@ while True:
         height = y + h
 
         # Drawing rectangle
-        if faces.all() == True:
-            cv.rectangle(frame, (x, y), (width, height),color, border)
+        if faces.all():
+            cv2.rectangle(frame, (x, y), (width, height),color, border)
+            cv2.putText(frame,"Face Found",(x,y-5),cv2.FONT_HERSHEY_SIMPLEX,0.9,color=color,thickness=2)
+
 
     # Display the result frame
-    cv.imshow('frame', frame)
+    cv2.imshow('Face Detection', frame)
 
-    if cv.waitKey(20) and 0xFF == ord('q'):
-        break
-
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
